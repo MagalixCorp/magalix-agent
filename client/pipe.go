5c529e0fd6e2ce5741718d07bf789da18d2fa558
Mohamed Elawadi <mohamed@elawadi.net>
2019-01-01 21:04:58 +0200

    pipe syncing

--- client/pipe.go
+++ client/pipe.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"sync"
+
 	"github.com/MagalixCorp/magalix-agent/proto"
 )
 
@@ -11,6 +13,8 @@ type PipeSender interface {
 
 // Pipe pipe
 type Pipe struct {
+	sync.Cond
+
 	sender  PipeSender
 	storage PipeStore
 }
@@ -25,7 +29,9 @@ func NewPipe(sender PipeSender) *Pipe {
 
 // Send pushes a packet to the pipe to be sent
 func (p *Pipe) Send(pack Package) int {
-	return p.storage.Add(&pack)
+	ret := p.storage.Add(&pack)
+	p.Broadcast()
+	return ret
 }
 
 // Start start sending packages
@@ -33,6 +39,10 @@ func (p *Pipe) Start() {
 	go func() {
 		for {
 			pack := p.storage.Peek()
+			if pack == nil {
+				p.Wait()
+				continue
+			}
 			err := p.sender.Send(pack.Kind, pack.Data, nil)
 			if err == nil {
 				p.storage.Ack(pack)
