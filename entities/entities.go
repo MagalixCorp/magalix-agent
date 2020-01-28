03651a179b0c50fcc5f80b4b5cacba0dd7b78cb3
iahvector <iah.vector@gmail.com>
2020-01-28 19:21:28 +0200

    DEV-538: Set resync ticker interval to 4 hours

--- entities/entities.go
+++ entities/entities.go
@@ -17,7 +17,7 @@ import (
 )
 
 const (
-	snapshotResyncTickerInterval = 30 * time.Minute
+	snapshotResyncTickerInterval = 4 * time.Hour
 	snapshotTickerInterval       = 3 * time.Hour
 
 	deltasBufferChanSize       = 1024
