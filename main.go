e788f944c071508d48fdc605b64c00cf141517b9
mohamed-mohaimen <mohamedveron23@gmail.com>
2020-07-23 18:43:03 +0200

    update profiling tool address

--- main.go
+++ main.go
@@ -313,7 +313,7 @@ func initAgent(args docopt.Opts, gwClient *client.Client, logger *log.Logger, ac
 	}
 
 	go func() {
-		http.ListenAndServe("localhost:6060", nil)
+		http.ListenAndServe(":6060", nil)
 	}()
 }
 
