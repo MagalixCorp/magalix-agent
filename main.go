c5f37b2c884253833c11bd51bb3258e153f70e1d
mohamed-mohaimen <mohamedveron23@gmail.com>
2020-07-24 15:48:08 +0200

    revert remove events and proc packages

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
+	"github.com/MagalixCorp/magalix-agent/v2/events"
 	"net/http"
 	"os"
 	"strings"
@@ -143,7 +144,7 @@ func main() {
 	logger.Infof(
 		karma.Describe("version", version).
 			Describe("args", fmt.Sprintf("%q", utils.GetSanitizedArgs())),
-		"magalix agent started...",
+		"magalix agent started.....",
 	)
 
 	secret, err := base64.StdEncoding.DecodeString(
@@ -199,7 +200,7 @@ func initAgent(args docopt.Opts, gwClient *client.Client, logger *log.Logger, ac
 	logger.Infof(nil, "Initializing Agent")
 	var (
 		metricsEnabled  = !args["--disable-metrics"].(bool)
-		//eventsEnabled   = !args["--disable-events"].(bool)
+		eventsEnabled   = !args["--disable-events"].(bool)
 		//scalarEnabled   = !args["--disable-scalar"].(bool)
 		executorWorkers = utils.MustParseInt(args, "--executor-workers")
 		dryRun          = args["--dry-run"].(bool)
@@ -280,7 +281,7 @@ func initAgent(args docopt.Opts, gwClient *client.Client, logger *log.Logger, ac
 		return nil, nil
 	})
 
-	/*if eventsEnabled {
+	if eventsEnabled {
 		events.InitEvents(
 			gwClient,
 			kube,
@@ -288,7 +289,7 @@ func initAgent(args docopt.Opts, gwClient *client.Client, logger *log.Logger, ac
 			entityScanner,
 			args,
 		)
-	}*/
+	}
 
 	if metricsEnabled {
 		var nodesProvider metrics.NodesProvider
