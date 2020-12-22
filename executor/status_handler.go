5fb232df7f51dc905e4b530c377534982ee0d60d
mohamed-mohaimen <mohamedveron23@gmail.com>
2020-12-23 00:10:52 +0200

    break when get the desired replica

--- executor/status_handler.go
+++ executor/status_handler.go
@@ -128,6 +128,7 @@ func (executor *Executor) deploymentsHandler(entityName string, namespace string
 		for _, replica := range currentReplicas {
 			if replica.readyReplicas > 0 {
 				deploymentName = replica.name
+				break
 			}
 		}
 	}
