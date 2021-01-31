26953a85e7c0d160af44d444edbd9a7acbe051fa
mohamed-mohaimen <mohamedveron23@gmail.com>
2021-01-31 21:16:49 +0200

    add 3 sec sleep after updating the deployment

--- executor/status_handler.go
+++ executor/status_handler.go
@@ -21,6 +21,8 @@ func (executor *Executor) podsStatusHandler(entityName string, namespace string,
 	var err error
 	flag := false
 
+	time.Sleep(3 * time.Second)
+
 	if strings.ToLower(kind) == "deployment" {
 		objectName, targetPods, err = executor.deploymentsHandler(entityName, namespace)
 		if err != nil {
@@ -130,7 +132,7 @@ func (executor *Executor) deploymentsHandler(entityName string, namespace string
 
 			// get the current replicaset with the same revision of the deployment
 			if replicaRevision == deploymentRevision {
-				logger.Debugw("deployment revision: " + deploymentRevision + "& replicaset revision: ", replicaRevision , "& replicaset revision: ", replicaRevision)
+				logger.Debugw("deployment revision: " + deploymentRevision + "& replicaset revision: " +  replicaRevision , "& replicaset revision: ", replicaRevision)
 				deploymentName = replica.Name
 				targetPods = *replica.Spec.Replicas
 				break
