26de994de0e4f3c6b02e52b7ffb8bcc2782e2955
ahsayde <ahmed.m.elsayed93@gmail.com>
2022-01-12 18:17:39 +0200

    fix tests

--- integration/tests/test_agent.py
+++ integration/tests/test_agent.py
@@ -11,14 +11,13 @@ PASSWORD = os.environ["PASSWORD"]
 CLUSTER_NAME = "agent-integration-test-" + uuid.uuid4().hex[:5]
 NAMESPACE = "agent-integration-test"
 AGET_IMAGE = os.environ["AGENT_IMAGE"]
-CATEGORY_ID = "magalix.categories.none"
 
 
 TEST_CONSTRAINTS_NAMES = [
-    "Using latest Image Tag",
-    "Services are not using ports over 1024",
-    "Missing Owner Label",
-    "Containers running with PrivilegeEscalation"
+    "Custom Policy - Using latest Image Tag",
+    "Custom Policy - Services are not using ports over 1024",
+    "Custom Policy - Missing Owner Label",
+    "Custom Policy - Containers running with PrivilegeEscalation"
 ]
 
 AGENT_YAML_PATH = "/tmp/agent_resources.yaml"
@@ -77,7 +76,7 @@ def create_test_policy(session, account_id):
     data = {
         "id": str(uuid.uuid4()),
         "name": "Test Policy " + uuid.uuid4().hex[:5],
-        "category": CATEGORY_ID,
+        "category": body["category_id"],
         "enabled": True,
         "targets": {
             "cluster": [],
@@ -89,23 +88,38 @@ def create_test_policy(session, account_id):
             "label": {"test": "agent.integration.test"}
         },
         "parameters": [
-        {
-            "name": "label",
-            "type": "string",
-            "required": True,
-            "default": "test-label",
-
-        },
-        {
-            "name": "value",
-            "type": "string",
-            "required": True,
-            "default": "test",
-
-        },
-    ],
+            {
+                "name": "label",
+                "type": "string",
+                "required": True,
+                "default": "test-label",
+            },
+            {
+                "name": "value",
+                "type": "string",
+                "required": True,
+                "default": "test",
+            },
+            {
+                "name": "exclude_namespace",
+                "type": "string",
+                "default": None,
+                "required": False
+            },
+            {
+                "name": "exclude_label_key",
+                "type": "string",
+                "default": None,
+                "required": False
+            },
+            {
+                "name": "exclude_label_value",
+                "type": "string",
+                "default": None,
+                "required": False
+            }
+        ]
     }
-
 
     body.update(data)
 
@@ -114,32 +128,48 @@ def create_test_policy(session, account_id):
 
     resp = session.get(URL + f"/api/{account_id}/policies/v1/policies/{resp.json()['id']}")
     assert resp.ok, "Failed to get created test policy"
-    return resp.json()
+    return body
 
 
-def update_test_policy(session, account_id, test_policy):
-    test_policy["parameters"] = [
+def update_test_policy(session, account_id, policy):
+    policy["parameters"] = [
         {
             "name": "label",
             "type": "string",
             "required": True,
             "default": "test-label-2",
-
         },
         {
             "name": "value",
             "type": "string",
             "required": True,
             "default": "test",
-
         },
+        {
+			"name": "exclude_namespace",
+			"type": "string",
+			"default": None,
+			"required": False
+		},
+		{
+			"name": "exclude_label_key",
+			"type": "string",
+			"default": None,
+			"required": False
+		},
+		{
+			"name": "exclude_label_value",
+			"type": "string",
+			"default": None,
+			"required": False
+		}
     ]
 
-    resp = session.put(URL + f"/api/{account_id}/policies/v1/policies/{test_policy['id']}", json=test_policy)
+    resp = session.put(URL + f"/api/{account_id}/policies/v1/policies/{policy['id']}", json=policy)
     assert resp.ok, "Failed to update test policy"
 
-def delete_test_policy(session, account_id, test_policy):
-    resp = session.delete(URL + f"/api/{account_id}/policies/v1/constraints/{test_policy['id']}")
+def delete_test_policy(session, account_id, policy_id):
+    resp = session.delete(URL + f"/api/{account_id}/policies/v1/constraints/{policy_id}")
     assert resp.ok, "Failed to delete test constraint"
 
 
@@ -207,47 +237,52 @@ class TestViolations:
         exit_code = os.system(f"kubectl apply -f {AGENT_YAML_PATH}")
         assert exit_code == 0, "Failed to run agent create deployment command"
 
-        time.sleep(360)
-
         yield account_id, cluster_id, session, test_policy, test_constraints
 
         exit_code = os.system(f"kubectl delete -f {AGENT_YAML_PATH}")
         assert exit_code == 0, "Failed to clean up agent deployment"
 
-        # resp = session.delete(URL + f"/api/accounts/v1/{account_id}/clusters/{cluster_id}")
-        # assert resp.ok, "Failed to delete cluster from console"
+        resp = session.delete(URL + f"/api/accounts/v1/{account_id}/clusters/{cluster_id}")
+        assert resp.ok, "Failed to delete cluster from console"
 
         try:
-            delete_test_policy(session, account_id, test_policy)
+            delete_test_policy(session, account_id, test_policy["id"])
         except:
             pass
 
     def test_agent_violations(self, create_cluster):
+        print("[+] Create cluster")
         account_id, cluster_id, session, test_policy, test_constraints = create_cluster
+        time.sleep(180)
 
+        print("[+] Check violations")
         for constraint in test_constraints:
             violations_count = get_constraint_violation_count(session, account_id, cluster_id, constraint["id"])
             assert violations_count == 1, "constraint: %s, expected 1 violation, but found %d" % (constraint["name"], violations_count)
 
-
+        print("[+] Apply resources fixes")
         exit_code = os.system(f"kubectl apply -f fixed_resources.yaml")
         assert exit_code == 0, "Failed to apply fixed resources environment"
 
-        time.sleep(200)
+        time.sleep(180)
 
+        print("[+] Check violations")
         for constraint in test_constraints:
             violations_count = get_constraint_violation_count(session, account_id, cluster_id, constraint["id"])
             assert violations_count == 0, "constraint: %s, expected 0 violations, but found %d" % (constraint["name"], violations_count)
 
+        print("[+] Update test policy")
         update_test_policy(session, account_id, test_policy)
-        time.sleep(200)
+        time.sleep(180)
 
+        print("[+] Check violations")
         violations_count = get_constraint_violation_count(session, account_id, cluster_id, test_policy["id"])
         assert violations_count == 1, f"expected 1 violations after updating test constraint, but found {violations_count}"
 
+        print("[+] Delete test policy")
+        delete_test_policy(session, account_id, test_policy["id"])
+        time.sleep(180)
 
-        delete_test_policy(session, account_id, test_policy)
-        time.sleep(200)
-
+        print("[+] Check violations")
         violations_count = get_constraint_violation_count(session, account_id, cluster_id, test_policy["id"])
         assert violations_count == 0, f"expected 0 violations after deleting test constraint, but found {violations_count}"
